dd1d740f236e0dd8fbe995daf72107e906228ee8
Robert Nelson <robert.nelson@stateless-software.com>
2014-03-08 16:14:13 -0600

    Fixed value string method.

--- src/main/java/com/apruve/PaymentRequest.java
+++ src/main/java/com/apruve/PaymentRequest.java
@@ -1,5 +1,8 @@
 package com.apruve;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +13,10 @@ import net.sf.json.processors.PropertyNameProcessor;
 import net.sf.json.util.PropertyFilter;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
 /**
  * A request to a payer for payment on behalf of a shopper. In order to provide
@@ -21,8 +28,13 @@ import org.apache.commons.lang.StringUtils;
  */
 public class PaymentRequest implements Serializable {
 	private static final long serialVersionUID = -4892365561562700036L;
+
 	public static final PropertyNameProcessor JSON_PROP_NAME_PROCESSOR;
 
+	private static final String PAYMENT_REQUEST_PATH = "/api/v3/payment_requests/%s";
+	private static final String FINALIZE_PAYMENT_REQUEST_PATH = "/api/v3/payment_requests/%s/finalize";
+
+	private String id;
 	private String merchantId;
 	private Integer amountCents = null;
 	private String currency = null;
@@ -62,6 +74,7 @@ public class PaymentRequest implements Serializable {
 	public PaymentRequest() {
 		this.merchantId = ApruveMerchant.getInstance().getMerchantId();
 	}
+
 
 	/**
 	 * Returns the JSON string for a PaymentRequest.
@@ -173,7 +186,8 @@ public class PaymentRequest implements Serializable {
 
 		for (LineItem line : lineItems) {
 			buf.append(line.getTitle());
-			buf.append(line.getAmountCents());
+			if (line.getAmountCents() != null)
+				buf.append(line.getAmountCents());
 			if (line.getPriceEachCents() != null)
 				buf.append(line.getPriceEachCents());
 			if (line.getQuantity() != null)
@@ -185,5 +199,80 @@ public class PaymentRequest implements Serializable {
 			buf.append(StringUtils.defaultString(line.getViewProductUrl()));
 		}
 		return buf.toString();
+	}
+
+
+	public static String getFinalizeUrl(String paymentRequestId)
+	{
+		String url = FINALIZE_PAYMENT_REQUEST_PATH.replaceAll("%s", paymentRequestId);
+		return ApruveClient.getInstance().getEnvironment().getBaseUrl() + url;
+	}
+
+
+	/**
+	 * @return the id
+	 */
+	public String getId()
+	{
+		return id;
+	}
+
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id)
+	{
+		this.id = id;
+	}
+
+
+	public static PaymentRequest finalize(String paymentRequestId, String apiKey)
+	{
+		try
+		{
+			CloseableHttpClient httpclient = HttpClients.createDefault();
+
+			try
+			{
+				HttpPost post = new HttpPost(getFinalizeUrl(paymentRequestId));
+				post.setHeader("Content-Type", "application/json");
+				post.setHeader("Apruve-Api-Key", apiKey);
+				HttpResponse response = httpclient.execute(post);
+				// TODO - Need error handling for 400 error response
+				BufferedReader rd = new BufferedReader(new InputStreamReader(
+						response.getEntity().getContent()));
+
+				String line = "";
+
+				while ((line = rd.readLine()) != null)
+				{
+					// Parse our JSON response
+					JSONObject json = JSONObject.fromObject(line);
+					PaymentRequest paymentRequest = new PaymentRequest();
+					paymentRequest.setAmountCents(json.get("amount_cents") != null ? new Integer(json.getString("amount_cents")) : null);
+					paymentRequest.setCurrency(json.getString("currency"));
+					paymentRequest.setId(json.getString("id"));
+					paymentRequest.setRecurring(json.get("recurring") != null ? new Boolean(json.getString("recurring")) : null);
+					paymentRequest.setShippingCents(json.get("shipping_cents") != null ? new Integer(json.getString("shipping_cents")) : null);
+					paymentRequest.setTaxCents(json.get("tax_cents") != null ? new Integer(json.getString("tax_cents")) : null);
+					return paymentRequest;
+				}
+
+				return null;
+			}
+			catch (Exception e)
+			{
+				throw new ApruveException("Unable to finalize Payment Request", e);
+			}
+			finally
+			{
+				httpclient.close();
+			}
+		}
+		catch (IOException e)
+		{
+			throw new RuntimeException("Exception while closing HTTPClient", e);
+		}
 	}
 }
